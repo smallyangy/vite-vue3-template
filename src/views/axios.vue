d5ce678a37dba9cff0a7c645411dfd0a3c354aff
DESKTOP-KL0E6A7\hjt-zy <315862477@qq.com>
2021-04-25 09:34:37 +0800

    feat(view): 我的介绍页面

--- src/views/axios.vue
+++ src/views/axios.vue
@@ -1,23 +1,50 @@
 <template>
-    <div class="title">
+    <div v-loading="loading" class="title">
         <h2>axios</h2>
+        <h3>个人信息</h3>
+        <div class="user-cell">
+            <img class="img" :src="user.avatar" />
+            <div class="info">
+                <div class="name">{{ user.name }}</div>
+                <div class="website">
+                    <a :href="user.html_url" target="_blank">{{ user.html_url }}</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-import { defineComponent } from 'vue';
+<script lang="ts">
+import { defineComponent, ref, onMounted, reactive } from 'vue';
 import axios from '../utils/axios';
 
 export default defineComponent({
     setup() {
-        axios
-            .get('/users/XPoet')
-            .then((res) => {
-                console.log('res: ', res);
-            })
-            .catch((err) => {
-                console.log('err: ', err);
-            });
+        const loading = ref(false);
+        const user = reactive({
+            name: '',
+            avatar: '',
+            html_url: ''
+        });
+
+        onMounted(async () => {
+            try {
+                loading.value = true;
+                const { data } = await axios.get('/users/smallyangy');
+                user.name = data.name;
+                user.avatar = data.avatar_url;
+                user.html_url = data.html_url;
+            } catch (e) {
+                console.error(e);
+            } finally {
+                loading.value = false;
+            }
+        });
+
+        return {
+            loading,
+            user
+        };
     }
 });
 </script>
@@ -26,6 +53,29 @@ export default defineComponent({
 .title {
     h2 {
         color: orange;
+    }
+    .user-cell {
+        display: flex;
+        align-items: center;
+        .img {
+            width: 60px;
+            height: 60px;
+            border-radius: 50%;
+            flex: none;
+        }
+        .info {
+            flex: 1;
+            margin-left: 20px;
+            .name {
+                font-weight: bold;
+            }
+            .website {
+                font-size: 12px;
+                a {
+                    color: #999;
+                }
+            }
+        }
     }
 }
 </style>
